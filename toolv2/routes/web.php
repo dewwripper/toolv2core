f8cbf474c2f1f44d51029d1633c7e73efb5c95c1
dewwripper <deww.ripper@gmail.com>
2021-04-19 07:10:59 +0700

    Core project

---
+++ toolv2/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'DashboardController@index');
+Route::get('/home', 'DashboardController@index');
+
+
+
+Route::post('register', 'Auth\AuthenticateController@register');
+Route::post('login', 'Auth\AuthenticateController@login');
